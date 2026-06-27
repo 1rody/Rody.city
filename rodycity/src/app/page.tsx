efe374f79c38b1192195e96bf10acfa716e4b4fa
1rody <rody.development@atomicmail.io>
2026-06-27 09:22:33 -0300

    <feat>(landing) v1.2.4  melhoria no lightmode do site, correcao de  problemas de ui da versao anterior 1.0

--- rodycity/src/app/page.tsx
+++ rodycity/src/app/page.tsx
@@ -2,7 +2,6 @@
 import Navigation from "@/src/components/navigation/header/navbar"
 import Hero from "@/src/components/pages/landing/hero"
 import About from "@/src/components/pages/landing/about"
-import Transition from "@/src/components/pages/landing/transition"
 import Featured from "@/src/components/pages/landing/featured"
 import Footer from "@/src/components/footer/footer";
 
