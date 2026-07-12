78f0e7408f3031ff522a8f980a165c6546bd7bb3
1rody <rody.development@atomicmail.io>
2026-07-12 18:47:38 -0300

    <feat> (about) criada seccao de summary + repach das cores

--- rodycity/src/components/pages/landing/about.tsx
+++ rodycity/src/components/pages/landing/about.tsx
@@ -1,7 +1,7 @@
 'use client';
 import '@/src/styles/pages/landing/about.css'
 
-import { motion, useScroll, useTransform} from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Link from 'next/dist/client/link';
 import Image from 'next/image'
 
@@ -11,44 +11,58 @@ export default function About() {
 
     return (
         <>
-        <Image width={3000} height={0} className='z-20 transition-primary' src="/assets/misc/transition2.svg" alt="transition-svg" />
-        <section id="about" className="flex z-10 material-white items-center justify-center flex-col">
-            <motion.h1 style={{x }} initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className='text-(length:--super-large-text-secondary)  font-(family-name:--font-Handjet)   text-center mt-30 mb-45'>Design is my language. </motion.h1>
-            <div className='w-full flex items-center justify-center'>
-                <motion.article style={{x }} initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className='card rounded-xl transition-transform cursor-cell flex flex-col items-center p-8  gap-8 justify-center'>
-                    <div className='flex font-(family-name:--font-Chakra) flex-col text-center'>
-                        <img src="/assets/misc/RodyLogo.png" alt="Profile Image" width="400" height="100" />
-                        <h2  className='font-black lg:text-sm md:text-sm mt-5 text-sm sm:text-sm' >[Front-end/ UI/UX ]</h2>
-                    </div>
-                    <div className='flex relative flex-wrap container-stacks items-center justify-center gap-2'>
-                        <p className='tech-2'>NextJs</p>
-                        <p className='tech-4'>Figma</p>
-                        <p className='tech-3 '>React</p>
-                        <p className='tech-4'>Typescript</p>
-                        <p className='tech-2'>Html/CSS</p>
-                        <p className='tech-5 '>JAVA</p>
-                    </div>
-                    <div>
-                        <h3 className=' text-center md:text-left sm:text-left lg:text-left p-4 text-(lenght:--small-text) font-(family-name:--font-jetbrains-mono)'>
-                            I am a Software Engineering student at UCSal and a Front-End Developer & UI/UX Designer. <br /><br />
-                            Bridging the gap between clean code and seamless user experiences, I craft modern web applications using using the above technologies.
-                            Exploring lightweight protocols like HTMX, I build pixel-perfect, high-performance interfaces with clean, purposeful code.
-                        </h3>
-                        <div className='flex w-full items-center justify-center mt-8  p-5'>
-                            <button className='dashed'>
-                                <Link href="/assets/docs/curriculum.pdf" download>Download curriculum</Link>
-                            </button>
+            <Image width={3000} height={0} className='z-20 transition-primary' src="/assets/misc/transition2.svg" alt="transition-svg" />
+            <section id="about" className="flex z-10 material-white items-center justify-center flex-col">
+                <div className='flex sm:max-w-4/5 md:max-w-5/5 lg:max-w-4/7 w-full pb-50  flex-col p-10 dashed-stroke-tertiary'>
+                    <motion.h1 style={{ x }} initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className='text-5xl font-(family-name:--font-Handjet)  lg:text-left md:text-left text-center mt-30 mb-5'>Design is my language. </motion.h1>
+                    <div className='flex flex-wrap lg:flex-nowrap gap-5 w-full'>
+                        <article className='bg-(--background) p-10 rounded-3xl '>
+                            <h2 className='text-(length:--super-large-text-secondary) font-black'>RODY</h2>
+                            <p className='text-xl'>
+                                I am a Software Engineering student at UCSal and a Front-End Developer & UI/UX Designer.
+                                <br />
+                                Bridging the gap between clean code and seamless user experiences, I craft modern web applications using using the above technologies. Exploring lightweight protocols like HTMX, I build pixel-perfect, high-performance interfaces with clean, purposeful code.
+                            </p>
+                        </article>
+                        <div className='flex flex-col justify-between w-5/5 '>
+                            <div className='flex mb-20 flex-col gap-5'>
+                                <Link className='text-3xl text-black ' href="#">Github</Link>
+                                <Link className='text-3xl text-black' href="#">Linkedin</Link>
+                            </div>
+                            <div className='mb-10  flex flex-col text-black gap-10'>
+                                <details className='modal-group border-b border-(--background)/20'>
+                                    <summary className='flex cursor-pointer list-none font-semibold text-lg items-center justify-between w-full'>
+                                        Lorem ypsun
+                                        <span className="ml-4 transition-all duration-500 group-open:rotate-45">+</span>
+                                    </summary>
+                                    <p>
+                                        Content
+                                    </p>
+                                </details>
+                                <details className='modal-group border-b border-(--background)/20'>
+                                    <summary className='flex cursor-pointer list-none font-semibold text-lg items-center justify-between w-full'>
+                                        Lorem ypsun
+                                        <span className="ml-4 transition-all duration-500 group-open:rotate-45">+</span>
+                                    </summary>
+                                    <p>
+                                        Content
+                                    </p>
+                                </details>
+                                <details className='modal-group border-b border-(--background)/20'>
+                                    <summary className='flex cursor-pointer list-none font-semibold text-lg items-center justify-between w-full'>
+                                        Lorem ypsun
+                                        <span className="ml-4 transition-all duration-500 group-open:rotate-45">+</span>
+                                    </summary>
+                                    <p>
+                                        Content
+                                    </p>
+                                </details>
+                            </div>
                         </div>
                     </div>
-                    <div>
-
-                    </div>
-                </motion.article>
-            </div>
-            <motion.h1  style={{x }} initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} className='text-(length:--super-large-text-secondary)  font-(family-name:--font-Handjet) text-center pb-40'>code is my syntax.</motion.h1>
-
-        </section>
-            <Image className="z-20 no-select transition-tertiary" src="/assets/misc/transition-3.svg" alt="Transition triangle" width={3000} height={0}/>
+                </div>
+            </section>
+            <Image className="z-20 no-select transition-tertiary" src="/assets/misc/transition-3.svg" alt="Transition triangle" width={3000} height={3} />
 
         </>
     )
