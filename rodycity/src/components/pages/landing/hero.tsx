6bc4b13a797fc1a335ae898bb3676554ff367846
1rody <rody.development@atomicmail.io>
2026-07-23 02:48:30 -0300

    <feat> (hero) include text for DESKTOP envoriments in brand

--- rodycity/src/components/pages/landing/hero.tsx
+++ rodycity/src/components/pages/landing/hero.tsx
@@ -8,8 +8,10 @@ export default function Hero() {
 
     return (
         <section id="introduction" className="hero no-select h-screen flex flex-col w-full overflow-hidden">
-            <div className="flex items-center justify-center absolute w-full h-screen">
+            <div className="hover-show-group text-(--foreground)  text-6xl text-center flex-wrap  flex items-center justify-center absolute w-full h-screen">
+                <h1 className='nav-pc'>ENGINEER </h1>
                 <Image src="/assets/icons/RodyLogo.svg" className="hover:scale-125 duration-300" height={400} width={300} alt="" />
+                <h1 className='nav-pc'>WHAT'S NEXT.</h1>
             </div>
 
             <div className="absolute bottom-0 w-full lg:-mb-50 mb:-mb-25 sm:-mb-5 flex justify-center overflow-hidden">
