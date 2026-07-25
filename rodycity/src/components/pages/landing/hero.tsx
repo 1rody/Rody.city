5dc57910a127c27052fd1859a6c06697693dcb6f
1rody <rody.development@atomicmail.io>
2026-07-25 18:29:32 -0300

    <style> (hero) revisao da hero section

--- rodycity/src/components/pages/landing/hero.tsx
+++ rodycity/src/components/pages/landing/hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
             <div className="hover-show-group text-(--foreground)  text-6xl text-center flex-wrap  flex items-center justify-center absolute w-full h-screen">
                 <h1 className='nav-pc'>ENGINEER </h1>
                 <Image src="/assets/icons/RodyLogo.svg" className="hover:scale-125 duration-300" height={400} width={300} alt="" />
-                <h1 className='nav-pc'>WHAT'S NEXT.</h1>
+                <h1 className='nav-pc'>WHAT&apos;S NEXT.</h1>
             </div>
 
             <div className="absolute bottom-0 w-full lg:-mb-50 mb:-mb-25 sm:-mb-5 flex justify-center overflow-hidden">
