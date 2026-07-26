5054f161693b2e8874c3202ca0d9a07f8b238e43
1rody <rody.development@atomicmail.io>
2026-07-26 16:02:03 -0300

    <style> (redirect) novo style incorporado a pagina de papers remeendo a uma era old da internet

--- rodycity/src/app/redirect/page.tsx
+++ rodycity/src/app/redirect/page.tsx
@@ -8,34 +8,56 @@ import Image from 'next/image'
     name: string;
     slug: string;
     id: string;
+    post_date: string;
   }
 
 export default function Redirect() {
 
     const papers: Paper[] = [
-      { name: "How to setup a Minecraft server", slug: "mc-server", id: "1" },
-      { name: "Javascript and Typescript diferences", slug: "js-ts", id: "2" },
-      { name: "Why use async functions?", slug: "async", id: "3" },
-      { name: "What is the diference of UI and UX", slug: "ui-ux", id: "4" },
+      { name: "How setup a Minecraft server", slug: "mc-server", id: "1", post_date: "dd/mm/yyyy" },
+      { name: "JS/TS diferences", slug: "js-ts", id: "2", post_date: "dd/mm/yyyy" },
+      { name: "Why use async functions?", slug: "async", id: "3", post_date: "dd/mm/yyyy" },
+      { name: "the diference of UI and UX", slug: "ui-ux", id: "4", post_date: "dd/mm/yyyy" },
     ];
 
 
   return (
     <>
       <Sidebar />
-      <section className="h-scree flex flex-col items-center">
-        <div className="container-articles">
-          <ol className="flex gap-4 flex-col  flex-wrap h-screen items-center justify-center">
-            {papers.map((paper) => (
-              <li key={paper.id}>
-                <Link
-                  href={`/papers/${paper.slug}`}
-                  className="bg-foreground flex flex-col text-black p-4 text-center hover:scale-105 rounded-sm lg:w-2xl md:w-lg  w-fit transition-all duration-200">
-                  <span className="font-medium">{paper.name}</span>
-                </Link>
-              </li>
-            ))}
-          </ol>
+      <section className="h-scree font-(family-name:--font-jetbrains-mono) text-indigo-50 flex flex-col items-center">
+        <div className="container-articles max-w-2/5 text-sm h-screen backdrop-blur-3xl items-center justify-center text-left flex flex-col">
+          <nav className='w-full pr-5 pl-5 border-1 bg-indigo-950/30 border-gray-500 border-b-blue-950 flex items-center justify-between'>
+            <p className='text-sm text-indigo-300'>rody.city — ~/papers</p>
+            <p>[ ! ]</p>
+          </nav>
+          <div className='flex  border-1 border-gray-500'>
+            <article className='w-full p-5  gap-5 text-gray-400  flex flex-col'>
+              <h2><span className='text-indigo-500'>rody:~$</span> ./Search_papers</h2>
+              <pre className='scale-90 text-indigo-600'>{`
+  ██████╗  ██████╗ ██████╗ ██╗   ██╗
+  ██╔══██╗██╔═══██╗██╔══██╗╚██╗ ██╔╝
+  ██████╔╝██║   ██║██║  ██║ ╚████╔╝ 
+  ██╔══██╗██║   ██║██║  ██║  ╚██╔╝  
+  ██║  ██║╚██████╔╝██████╔╝   ██║   
+  ╚═╝  ╚═╝ ╚═════╝ ╚═════╝    ╚═╝
+            `}</pre>
+              <h3 className='bg-indigo-100 text-black flex w-fit underline'>read my papers ~</h3>
+              <p className='max-w-3/3'>
+                - Welcome :&gt;  to my papers page<br />  <br />  this is my space for sharing knowledge and everything  I learn along my development journey
+              </p>
+            </article>
+            <ol className="flex gap-3 w-full flex-col paper-cli border-b-1 flex-wrap items-left p-5 ">
+              {papers.map((paper) => (
+                <li key={paper.id}>
+                  <Link
+                    href={`/papers/${paper.slug}`}
+                    className="flex flex-col text-gray- w-fit  hover:gap-5  text-indigo-800 text-left transition-all duration-200">
+                    <span className="font-medium"> {paper.post_date} | <span className=' hover:bg-indigo-100 hover:text-black text-indigo-100'> [{paper.name}]</span></span>
+                  </Link>
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
       </section>
       <div className="shell-external  h-screen w-full hidden fixed lg:block md:block no-select no-interact">
