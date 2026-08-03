d7c956f8e97989c2e9c50130135004671aa2ac6b
Rody <131989043+1rody@users.noreply.github.com>
2026-08-03 13:35:59 -0300

    Fix bottom positioning of hero section elements

--- rodycity/src/components/pages/landing/hero.tsx
+++ rodycity/src/components/pages/landing/hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
                 <h1 className='nav-pc mt-46 text-8xl'>WHAT&apos;S NEXT.</h1>
             </div>
 
-            <div className="absolute -bottom-15 lg:bottom-5 md:-bottom-24 w-full lg:-mb-48 mb:-mb-25 sm:-mb-5 flex justify-center overflow-hidden">
+            <div className="absolute -bottom-15 lg:bottom-8 md:-bottom-24 w-full lg:-mb-48 mb:-mb-25 sm:-mb-5 flex justify-center overflow-hidden">
                 <motion.h1 initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }}transition={{ duration: 0.6 }}className="deploy-text no-interactions text-(length:--super-large-text) font-(family-name:--font-ClimateCrisis) font-black leading-none text-center"> DEPLOY </motion.h1>
             </div>
         </section>
