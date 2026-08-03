6739e349d7ee7a0dd4bfb9c5afce6eb90dfedb85
Rody <131989043+1rody@users.noreply.github.com>
2026-08-03 13:55:37 -0300

    Increase font size for hero section headings

--- rodycity/src/components/pages/landing/hero.tsx
+++ rodycity/src/components/pages/landing/hero.tsx
@@ -9,13 +9,13 @@ export default function Hero() {
     return (
         <section id="introduction" className="hero z-5 no-select h-screen flex flex-col w-full overflow-hidden">
             <div className="hover-show-group text-(--foreground) scale-95  text-center flex-wrap  flex items-center justify-center absolute w-full h-screen">
-                <h1 className='nav-pc mb-46 text-6xl font-(family-name:--font-ClimateCrisis)'>ENGINEER </h1>
+                <h1 className='nav-pc mb-46 text-7xl '>ENGINEER </h1>
                 <h1 className='nav-pc mb-0 absolute mr-150 text-xl'>CREATE </h1>
                 <h1 className='nav-pc mb-0 absolute ml-50 text-xl'>PUSH </h1>
                 <h1 className='nav-pc mt-0 absolute mr-240 text-xl'>PLAN </h1>
                 <h1 className='nav-pc  absolute ml-190 text-xl'>USE </h1>
                 <Image src="/assets/icons/RodyLogo.svg" className=" duration-300" height={400} width={300} alt="" />
-                <h1 className='nav-pc mt-46 text-6xl font-(family-name:--font-ClimateCrisis)'>WHAT&apos;S NEXT</h1>
+                <h1 className='nav-pc mt-46 text-7xl'>WHAT&apos;S NEXT</h1>
             </div>
 
             <div className="absolute -bottom-14 lg:bottom-8 md:-bottom-24 w-full lg:-mb-42 mb:-mb-25 sm:-mb-5 flex justify-center overflow-hidden">
