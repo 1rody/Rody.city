8614d8f3b2356270d85d5eb3461db100278a9ae2
1rody <rody.development@atomicmail.io>
2026-08-04 23:54:48 -0300

    <feat>(hero) implementado botoes e reacoes na herosection

--- rodycity/src/components/pages/landing/hero.tsx
+++ rodycity/src/components/pages/landing/hero.tsx
@@ -10,11 +10,11 @@ export default function Hero() {
         <section id="introduction" className="hero z-5 no-select h-screen  w-full flex flex-col w-full overflow-x-hidden">
             <div className="hover-show-group w-full text-(--foreground) resp-scale flex-nowrap text-center relative  overflow-x-hidden flex items-center justify-center absolute w-full h-screen">
                 <h1 className='nav-pc mb-46 lg:text-7xl md:text-5xl '>ENGINEER </h1>
-                <h2 className='nav-pc mb-0 absolute -translate-x-60 text-sm'>CREATE </h2>
-                <h2 className='nav-pc mb-0 absolute translate-x-25 text-sm'>PUSH </h2>
-                <h2 className='nav-pc mt-0 absolute -translate-x-90 text-sm'>PLAN </h2>
-                <h2 className='nav-pc absolute translate-x-60 text-sm'>USE </h2>
-                <Image src="/assets/icons/RodyLogo.svg" className=" duration-300" height={400} width={300} alt="" />
+                <h2 className='nav-pc mb-0 absolute -translate-x-60 text-sm hover:bg-red-500 duration-200 active:scale-95'>CREATE </h2>
+                <h2 className='nav-pc mb-0 absolute translate-x-25 text-sm hover:bg-red-500 active:scale-95'>PUSH </h2>
+                <h2 className='nav-pc mt-0 absolute -translate-x-90 text-sm hover:bg-red-500 duration-200 active:scale-95'>PLAN </h2>
+                <h2 className='nav-pc absolute translate-x-60 text-sm hover:bg-red-500 duration-200 active:scale-95'>USE </h2>
+                <Image src="/assets/icons/RodyLogo.svg" className="hover:scale-110 hover:rotate-12 duration-300" height={400} width={300} alt="" />
                 <h1 className='nav-pc mt-46 lg:text-7xl  md:text-5xl whitespace-nowrap'>WHAT&apos;S NEXT</h1>
             </div>
 
