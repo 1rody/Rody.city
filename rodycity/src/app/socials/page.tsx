e4df0e25ef683973175cb88fa3cbc4553a813923
1rody <rody.development@atomicmail.io>
2026-08-17 01:44:48 -0300

    <feat> (socials) bg chanfed & utility improvments on socials page

--- rodycity/src/app/socials/page.tsx
+++ rodycity/src/app/socials/page.tsx
@@ -1,93 +1,64 @@
 "use client";
 
-import { useState, useEffect } from 'react';
 import Link from 'next/dist/client/link';
+import Image from 'next/image'
 
 import '@/src/styles/components/ui/bg.css'
 import '@/src/styles/pages/socials/socials.css';
 
 export default function socials() {
 
-    const [discordStatus, setDiscordStatus] = useState({online:0, total: 0});
-    const [loading, setLoading] = useState(true);
 
-    useEffect(()=> {
-        fetch('/api/discord') 
-        .then((res) => res.json())
-        .then((data) => {
-            if(!data.error) {
-                setDiscordStatus({
-                    online: data.online,
-                    total: data.total,
-                });
-            };
-            setLoading(false);
-        }).catch((err) => {
-            console.error("Erro ao buscar dados do Discord:", err);
-            setLoading(false);
-        });
-    }, []); 
-  return (
-    <>
-    <img src="/assets/backgrounds/socials.gif" className='w-full h-screen blur-sm dark:opacity-40' alt="" />
-    <section id="socials-container" className="h-screen absolute z-2 scale-95 lg:scale-100 w-full flex items-center justify-center">
-        <div className="socials backdrop-blur-xl hover:scale-110 duration-200 glassmorphism-secondary items-center justify-center rounded-4xl flex flex-col">
-            <div className='flex flex-col w-full items-center justify-center p-3'>
-                <div className='text-center pt-10 gap-2'>
-                    <Link href="/" className='font-bold text-5xl font-(family-name:--font-geist)'>RODY</Link>
-                    <p className='font-(family-name:--font-jetbrains-mono)'><span>Welcome to my social tree!</span></p>
-                </div>
-            </div>
-            <article className='p-3 items-center justify-center w=full flex flex-col gap-5'>
-
-                <p className='mt-10 text-sm p-5 max-w-4/5 font-(family-name:--font-jetbrains-mono) rounded-2xl glassmorphism-secondary'>
-                    Hello, Im <strong>rody</strong>! <br /><br /> im a web developer and designer, I love to create web solutions and functional websites.  <br /> <br />I have a passion for learning new technologies and improving my skills.
-                </p>
-            </article>
-            <div className='flex mt-5 p-5 items-center justify-center w-full'>
-                <ol className='flex gap-2'>
-                    <li className='tech-1 w-full '>
-                        <Link href=''>Instagram</Link>
-                    </li>
-                    <li className='tech-2 w-full'>
-                        <Link href=''>X</Link>
-                    </li>
-                    <li className='tech-3 w-full'>
-                        <Link href=''>Github</Link>
-                    </li>
-                    <li className='tech-4 w-full'>
-                        <Link href=''>Linkedin</Link>
-                    </li>
-                </ol>
-            </div>
-            <div className='discord-container w-full  p-5 '>
-                <div className='flex flex-col p-4 rounded-3xl glassmorphism-secondary gap-5'>
-                    <div className='flex gap-2 items-center justify-between'>
-                        <img className='server-pic ' src="/assets/icons/RodyLogo.svg" alt="Camel Icon" />
-                        <div>
-                            <h3 className='text-2xl'>LOWN</h3>
-                            <div className='online-container flex flex-wrap lg:flex md:flex text-xs gap-3'>
-                                {loading ?  ( <span>Carregando...</span> ) :
-                                (
-                                    <>
-                                        <span>{discordStatus.online} online</span>
-                                        <p>of</p>
-                                        <span>{discordStatus.total} members</span>
-                                    </>
-                                )};
-                            </div>
+    return (
+        <>
+            <Image width={0} height={0} src="/assets/backgrounds/social.gif" className='w-full h-screen blur-sm dark:opacity-40' alt="Background image" />
+            <section id="socials-container" className="h-screen absolute z-2 scale-95 lg:scale-100 w-full flex items-center justify-center">
+                <div className="socials backdrop-blur-xl max-w-2xl   duration-200 glassmorphism-secondary items-center justify-center rounded-4xl flex flex-col">
+                    <div className='flex flex-col w-full items-center justify-center p-5'>
+                        <div className='text-center  gap-2'>
+                            <Image width={430} height={10} src="/assets/backgrounds/giphy.gif" className='relative -mb-22 h-28 z-10 rounded-2xl' alt="Banner image" />
+                            <Link href="/" className='font-bold text-5xl z-15 font-(family-name:--font-geist) relative'>RODY</Link>
+                            <p className=' z-15 font-(family-name:--font-jetbrains-mono) relative'><span>Welcome to my social tree!</span></p>
                         </div>
-                            <div className='p-1 text-xs'>
-                                <button className='discord-btn '><Link href=''>Dive in!</Link></button>
-                            </div>
                     </div>
+                    <ol className='flex w-full justify-around items-center  text-center p-4'>
+                        <li className='hover:bg-black hover:text-black hover:invert-100 bg-white/5 rounded-l-2xl p-2 items-center justify-center flex hover:rounded-full backdrop-blur-3xl w-full'>
+                            <Link href='https://www.instagram.com/1r0dy_?igsh=MXV1aTZzdHRuemwxZQ%3D%3D&igsi=MXV1aTZzdHRuemwxZQ%3D%3D&utm_source=qr'>
+                                <Image alt='Instagram icon' width={28} height={28} src="/assets/icons/icons8-instagram.svg"></Image>
+                            </Link>
+                        </li>
+                        <li className='hover:bg-black hover:text-black hover:invert-100 bg-white/5 p-2  duration-200  items-center justify-center flex hover:rounded-full backdrop-blur-3xl w-full'>
+                            <Link href='https://x.com/1R0_DY'>
+                                <Image alt='X/Twitter icon' width={28} height={28} src="/assets/icons/icons8-x.svg"></Image>
+                            </Link>
+                        </li>
+                        <li className='hover:bg-black hover:text-black hover:invert-100 bg-white/5 p-2  duration-200  items-center justify-center flex hover:rounded-full backdrop-blur-3xl w-full'>
+                            <Link href='https://github.com/1rody'>
+                                <Image alt='github icon' width={28} height={28} src="/assets/icons/icons8-github.svg"></Image>
+                            </Link>
+                        </li>
+                        <li className='hover:bg-black hover:text-black hover:invert-100  bg-white/5  duration-200  p-2 items-center justify-center flex hover:rounded-full backdrop-blur-3xl w-full'>
+                            <Link href='https://br.pinterest.com/1R0_DY/'>
+                                <Image alt='pinterest icon' width={28} height={28} src="/assets/icons/icons8-pinterest.svg"></Image>
+                            </Link>
+                        </li>
+                        <li className='hover:bg-black hover:text-black hover:invert-100 rounded-r-2xl duration-200  bg-white/5 p-2 items-center justify-center flex hover:rounded-full backdrop-blur-3xl w-full'>
+                            <Link href='https://www.linkedin.com/in/rodolfo-falc%C3%A3o-03b27a379/'>
+                                <Image alt='linkedin icon' width={28} height={28} src="/assets/icons/icons8-linkedin.svg"></Image>
+                            </Link>
+                        </li>
+                    </ol>
+                    <article className=' items-center justify-center w-full flex flex-col pb-10 gap-5'>
+
+                        <p className='mt-4 text-sm p-3 max-w-4/5 font-(family-name:--font-jetbrains-mono) rounded-2xl '>
+                            Hello, Im <strong>rody</strong>! <br /><br /> im a web developer and designer, I love to create web solutions and functional websites.  <br /> <br />I have a passion for learning new technologies and improving my skills.
+                        </p>
+                    </article>
                 </div>
-            </div>
-        </div>
-        < div className='absolute bottom-10 text-center text-white'>
-            <p className='text-sm'>© {new Date().getFullYear()} RODY LINKTREE. wanna build your own?  <Link className='text-blue-500' href='https://github.com/rodolfoalves/rodycity' target='_blank' rel='noopener noreferrer'>click here</Link></p>
-        </div>
-    </section>
-    </>
-  );
+                < div className='absolute bottom-10 text-center text-white'>
+                    <p className='text-sm'>© {new Date().getFullYear()} RODY LINKTREE. wanna build your own?  <Link className='text-blue-500' href='https://github.com/rodolfoalves/rodycity' target='_blank' rel='noopener noreferrer'>click here</Link></p>
+                </div>
+            </section>
+        </>
+    );
 }
--- rodycity/src/components/pages/landing/hero.tsx
+++ rodycity/src/components/pages/landing/hero.tsx
@@ -5,7 +5,12 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 
 export default function Hero() {
+    //resumo para o rody do futuro (no motion0 pra criar o efeito parlax do h1 isei a const Scrollyprogress chavrada peganod os atributos do useScroll do motion)
+    //lendo o scroll
+    const  { scrollYProgress } = useScroll();
 
+    // e depois de definir isso eu mapeio a rolagem com a const abaixo usando o scroll lido acima
+    const y = useTransform(scrollYProgress, [0, 0.2], [0, 150]);
     return (
         <section id="introduction" className="hero  fixed z-5 no-select h-screen  w-full flex flex-col w-full items-center overflow-x-hidden">
             <div className="hover-show-group  text-center relative overflow-x-hidden flex items-center justify-center h-screen w-full p-10">
@@ -15,7 +20,7 @@ export default function Hero() {
                     transition={{ duration: 0.6 }}
                     className=" no-interactions text-(length:--super-large-text) font-(family-name:--font-ClimateCrisis) w-full h-full flex rounded-2xl justify-center items-center lg:border border-amber-50/20 font-black leading-none"
                 >
-                    RODY
+                    <motion.span  style={{y}}>RODY</motion.span>
                 </motion.h1>
             <div className="absolute hidden lg:flex font-(family-name:--font-geist)  font-black bottom-15 right-15 text-right gap-10">
                 <Link className="text-lg hover:scale-95 hover:text-black hover:bg-white p-2 rounded-4xl active:border-b-1 hover:opacity-150 transition-all  duration-120 " href='/homebrew'>PAPERS</Link>
