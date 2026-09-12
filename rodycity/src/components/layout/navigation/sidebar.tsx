e8c5c127552f06f52f75460c60a80596a32d2fb9
1rody <development.rodolfo@atomicmail.io>
2026-09-12 11:48:26 -0300

    <feat> (linktreecreation) crating a page for generate your own linktree in rody.city model.

--- rodycity/src/components/layout/navigation/sidebar.tsx
+++ rodycity/src/components/layout/navigation/sidebar.tsx
@@ -8,7 +8,7 @@ export default function Sidebar() {
             <div className="flex flex-col w-full items-center justify-center p-8">
                 <h2 className="text-2xl font-bold mb-4"> UNSEEN.TREE</h2>
             </div>
-            <ol classsname="flex flex-col w-full justify-around items-center  text-center p-4 backdrop-blur-xl glassmorphism-primary">
+            <ol className="flex flex-col w-full justify-around items-center  text-center p-4 backdrop-blur-xl glassmorphism-primary">
             </ol>                
             <div className="flex flex-col w-full items-center gap-3 justify-center p-8">
                 <Link className="bg-white rounded-xl p-2 w-full text-center text-black" href="/">Download your Linktree.</Link>
